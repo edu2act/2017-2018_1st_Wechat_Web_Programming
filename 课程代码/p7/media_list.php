73f0450e1ff870ebdd4136f4af7292de20c7c461
master-genius <1146040444@qq.com>
2017-09-06 09:49:21 +0800

    --

--- 课程代码/p7/media_list.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require ('../../vendor/autoload.php');
-
-$wxm = new weixin\wxMedia;
-
-walk_arr_echo(json_decode($wxm->getMediaList('image'),true));
-
-walk_arr_echo(json_decode($wxm->getMediaList('thumb'),true));
